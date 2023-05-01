07de6f1e47c7e3b5de813f6d771187112d941038
ElinaUsaeva <Usaeva1@yandex.ru>
2023-05-01 19:52:23 +0300

    Fix #1 #2 #3 #4 #5 #6

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -5,17 +5,15 @@ import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
-    @Test
-    public void shouldAddLessThanMaxBalance() {
-        SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
-                5
-        );
-
-        account.add(3_000);
-
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    @Test // исключение для процентной ставки
+    public void NegativeRate() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    2_000,
+                    1_000,
+                    10_000,
+                    -5
+            );
+        });
     }
 }
